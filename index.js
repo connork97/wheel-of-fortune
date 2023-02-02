f73a3928f0cd909205aa79e24c6bb51dde2df344
Connor Kormos <connorkormos97@gmail.com>
2023-02-02 15:45:37 -0800

    added player name text box and submit button.  not yet functional

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ spinBtn.onclick = function() {
     wheel.style.transform = "rotate(" + value + "deg)";
     value += Math.ceil(Math.random() * 3600);
 }
+
+function getName() {
+    var val = document.getElementById("name").value;
+    localStorage.setItem("Player 2", val);
+}
