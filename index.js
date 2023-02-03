69d0417f10dfacbf759a248f9af0e7eff7a4fddc
Connor Kormos <connorkormos97@gmail.com>
2023-02-02 18:49:34 -0800

    text input now allows user to customize name on wheel.  thanks simon

--- index.js
+++ index.js
@@ -7,7 +7,51 @@ spinBtn.onclick = function() {
     value += Math.ceil(Math.random() * 3600);
 }
 
-function getName() {
-    var val = document.getElementById("name").value;
-    localStorage.setItem("Player 2", val);
+const playerArr = ["player-1", "player-5", "player-3", "player-7", "player-2", "player-6", "player-8", "player-4"];
+let playerIndex = 0;
+
+function addName() {
+    if (playerIndex <= playerArr.length) {
+        let userName = document.getElementById('name').value;
+        document.querySelector(`#${playerArr[playerIndex]} span`).innerHTML = userName;
+        playerIndex++;
+    }
 }
+let submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click',addName);
+
+
+/*
+function getName() {
+    newName.innerHTML = userName.value;
+}
+
+submitBtn.addEventListener('click',addName);
+
+
+/*
+
+const oldPlayer = document.getElementById("player-1").value;
+const newPlayer = document.getElementById("name-box").value
+function addPlayer() {
+    oldPlayer.replace(/Spin Again/, newPlayer.value);
+    //document.getElementById("player-1").innerHTML = newPlayer;
+}
+
+/*function addPlayer() {
+    for (let i = 1; i < 9; i++) {
+
+    }
+}
+
+----------
+
+const name = document.getElementById("name").value;
+const oldName = document.getElementById("player-1").value;
+const newName = oldName.replace(/player-1/, "name");
+
+function getName() {
+    var newPlayerName = document.getElementById("name").value;
+    localStorage.setItem("player.span", newName);
+}
+*/
